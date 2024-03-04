124acc94cb322bc7085a800496b4dce290e51085
Roann Cantel <roann.cantel@telecom-paris.fr>
2024-03-04 09:40:49 +0100

    Ajout du TP03

---
+++ 03_tsconfig.json/src/moduleTravail.ts
@@ -0,0 +1,6 @@
+// @filename: moduleTravail.ts
+
+export function calculerJoursRestants(nb_jours_pris: number)
+{
+	return (50-nb_jours_pris);
+}
---
+++ 03_tsconfig.json/src/moduleVacances.ts
@@ -0,0 +1,6 @@
+// @filename: moduleVacances.ts
+
+export function calculerJoursRestants(nb_jours_pris: number)
+{
+	return (1-nb_jours_pris);
+}
