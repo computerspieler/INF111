ecd8763ff156ba426b02fd3a12c37aaff6e02fda
Roann Cantel <roann.cantel@telecom-paris.fr>
2024-03-07 10:20:11 +0100

    TP05: Défi 7

--- 05_html_css_2048/src/main.ts
+++ 05_html_css_2048/src/main.ts
@@ -12,20 +12,33 @@ function testBonjour(): void {
 }
 */
 
+let score: number = 0;
+function updateScore(): void {
+	document.getElementById("score").textContent = score.toString();
+}
+
 function haut(): void {
 	console.log("haut");
+	score += 1;
+	updateScore();
 }
 
 function bas(): void {
 	console.log("bas");
+	score += 1;
+	updateScore();
 }
 
 function gauche(): void {
 	console.log("gauche");
+	score += 1;
+	updateScore();
 }
 
 function droite(): void {
 	console.log("droite");
+	score += 1;
+	updateScore();
 }
 
 document.body.addEventListener('keydown', (ev) => {
