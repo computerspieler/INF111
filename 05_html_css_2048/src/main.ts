e14e9e145718a6f671a6123fc6cc7e0cb7879318
Roann Cantel <roann.cantel@telecom-paris.fr>
2024-03-07 10:20:36 +0100

    TP05: Défi 10 + Ajout d'un moyen de "nettoyer" une case

--- 05_html_css_2048/src/main.ts
+++ 05_html_css_2048/src/main.ts
@@ -45,17 +45,6 @@ function droite(): void
 	updateScore();
 }
 
-document.body.addEventListener('keydown', (ev) => {
-	if(ev.key == 'ArrowUp')
-		haut();
-	else if(ev.key == 'ArrowDown')
-		bas();
-	else if(ev.key == 'ArrowLeft')
-		gauche();
-	else if(ev.key == 'ArrowRight')
-		droite();
-});
-
 function getCell(i: number, j: number): HTMLTableCellElement | undefined
 {
 	const table: HTMLTableElement = document.getElementById("game") as HTMLTableElement;
@@ -85,7 +74,10 @@ function setValue(i: number, j: number, value: number): boolean
 	if(j >= table.rows[i].cells.length)
 		return false;
 
-	table.rows[i].cells[j].textContent = value.toString();
+	if(isNaN(value))
+		table.rows[i].cells[j].textContent = "*";
+	else
+		table.rows[i].cells[j].textContent = value.toString();
 	return true;
 
 }
@@ -107,13 +99,221 @@ function isEmpty(i: number, j: number): boolean
 	return isNaN(Number(cell.textContent));
 }
 
-// Tests
-const y = 2;
-const x = 1;
-const val = 10;
-console.assert(isNaN(getValue(y, x)), "Data preinitalized");
-setValue(y, x, val);
-console.assert(getValue(y, x) == val, "Data non-modified");
+function newGame(): void
+{
+	const table: HTMLTableElement = document.getElementById("game") as HTMLTableElement;
+	const height = table.rows.length;
+	const width = table.rows[0].cells.length;
 
-console.assert(!isEmpty(y, x), "Data non-modified");
-console.assert(isEmpty(y, x+1), "Invalid condition");
+	// On nettoie le terrain de jeu
+	for(let i = 0; i < height; i ++)
+		for(let j = 0; j < width; j ++)
+			setValue(i, j, NaN);
+
+	// On initialise la valeur des cases
+	const v1 = Math.random() >= 0.85 ? 4 : 2;
+	const v2 = Math.random() >= 0.86 ? 4 : 2;
+
+	// On initialise la position des cases
+	const x1 = Math.floor(Math.random() * width);
+	const y1 = Math.floor(Math.random() * height);
+	
+	let x2 : number = x1;
+	let y2 : number = y1;
+
+	while(x2 == x1 && y2 == y2) {
+		x2 = Math.floor(Math.random() * width);
+		y2 = Math.floor(Math.random() * height);
+	}
+
+	setValue(y1, x1, v1);
+	setValue(y2, x2, v2);
+}
+
+function moveRight(i: number): boolean
+{
+	const table = document.getElementById("game") as HTMLTableElement;
+	const height = table.rows.length;
+	const width = table.rows[0].cells.length;
+	
+	if(i < 0 || i > height)
+		return false;
+
+	let output = false;
+	
+	for(let j = 0; j < width - 1; j ++) {
+		if(isEmpty(i, j+1) && !isEmpty(i, j)) {
+			output = true;
+			setValue(i, j+1, getValue(i, j));
+			setValue(i, j, NaN);
+		}
+	}
+
+	if(!output)
+		return false;
+
+	moveRight(i);
+	return true;
+}
+
+function moveLeft(i: number): boolean
+{
+	const table = document.getElementById("game") as HTMLTableElement;
+	const height = table.rows.length;
+	const width = table.rows[0].cells.length;
+	
+	if(i < 0 || i > height)
+		return false;
+
+	let output = false;
+	
+	for(let j = width - 1; j > 0; j --) {
+		if(isEmpty(i, j-1) && !isEmpty(i, j)) {
+			output = true;
+			setValue(i, j-1, getValue(i, j));
+			setValue(i, j, NaN);
+		}
+	}
+
+	if(!output)
+		return false;
+	
+	moveLeft(i);
+	return true;
+}
+
+function moveUp(j: number): boolean
+{
+	const table = document.getElementById("game") as HTMLTableElement;
+	const height = table.rows.length;
+	const width = table.rows[0].cells.length;
+	
+	if(j < 0 || j > width)
+		return false;
+
+	let output = false;
+	
+	for(let i = height - 1; i > 0; i --) {
+		if(isEmpty(i-1, j) && !isEmpty(i, j)) {
+			output = true;
+			setValue(i-1, j, getValue(i, j));
+			setValue(i, j, NaN);
+		}
+	}
+
+	if(!output)
+		return false;
+	moveUp(j);
+	return true;
+}
+
+function moveDown(j: number): boolean
+{
+	const table = document.getElementById("game") as HTMLTableElement;
+	const height = table.rows.length;
+	const width = table.rows[0].cells.length;
+	
+	if(j < 0 || j > width)
+		return false;
+
+	let output = false;
+	
+	for(let i = 0; i < height - 1; i ++) {
+		if(isEmpty(i+1, j) && !isEmpty(i, j)) {
+			output = true;
+			setValue(i+1, j, getValue(i, j));
+			setValue(i, j, NaN);
+		}
+	}
+
+	if(!output)
+		return false;
+	moveDown(j);
+	return true;
+}
+
+function tests()
+{
+	const y = 2;
+	const x = 1;
+	const val = 10;
+	console.assert(isNaN(getValue(y, x)), "Data preinitalized");
+	setValue(y, x, val);
+	console.assert(getValue(y, x) == val, "Data non-modified");
+
+	console.assert(!isEmpty(y, x), "Data non-modified");
+	console.assert(isEmpty(y, x+1), "Invalid condition");
+
+	moveRight(y);
+	console.assert(isNaN(getValue(y, x)), "moveRight");
+	console.assert(!isNaN(getValue(y, 3)), "moveRight");
+
+	moveLeft(y);
+	console.assert(isNaN(getValue(y, 3)), "moveLeft");
+	console.assert(!isNaN(getValue(y, 0)), "moveLeft");
+
+	moveUp(0);
+	console.assert(isNaN(getValue(y, 0)), "moveUp");
+	console.assert(!isNaN(getValue(0, 0)), "moveUp");
+
+	moveDown(0);
+	console.assert(isNaN(getValue(0, 0)), "moveDown");
+	console.assert(!isNaN(getValue(3, 0)), "moveDown");
+
+	setValue(3, 0, NaN);
+	console.assert(isNaN(getValue(3, 0)), "Data non de-initialized");
+
+	// Exemple 1
+	setValue(0, 2, 2);
+	moveRight(0);
+	console.assert( isNaN(getValue(0, 0)), "exemple1_1", getValue(0, 0));
+	console.assert( isNaN(getValue(0, 1)), "exemple1_2", getValue(0, 1));
+	console.assert( isNaN(getValue(0, 2)), "exemple1_3", getValue(0, 2));
+	console.assert(!isNaN(getValue(0, 3)), "exemple1_4", getValue(0, 3));
+
+	// Exemple 2
+	setValue(1, 0, 4);
+	setValue(1, 2, 2);
+	moveRight(1);
+	console.assert( isNaN(getValue(1, 0)), "exemple2_1", getValue(1, 0));
+	console.assert( isNaN(getValue(1, 1)), "exemple2_2", getValue(1, 1));
+	console.assert(!isNaN(getValue(1, 2)), "exemple2_3", getValue(1, 2));
+	console.assert(!isNaN(getValue(1, 3)), "exemple2_4", getValue(1, 3));
+
+	// Exemple 3
+	setValue(2, 0, 2);
+	setValue(2, 2, 2);
+	setValue(2, 3, 2);
+	moveRight(2);
+	console.assert( isNaN(getValue(2, 0)), "exemple3_1", getValue(2, 0));
+	console.assert(!isNaN(getValue(2, 1)), "exemple3_2", getValue(2, 1));
+	console.assert(!isNaN(getValue(2, 2)), "exemple3_3", getValue(2, 2));
+	console.assert(!isNaN(getValue(2, 3)), "exemple3_4", getValue(2, 3));
+
+	// Exemple 4
+	setValue(3, 0, 4);
+	setValue(3, 1, 2);
+	setValue(3, 3, 4);
+	moveRight(3);
+	console.assert( isNaN(getValue(3, 0)), "exemple4_1", getValue(3, 0));
+	console.assert(!isNaN(getValue(3, 1)), "exemple4_2", getValue(3, 1));
+	console.assert(!isNaN(getValue(3, 2)), "exemple4_3", getValue(3, 2));
+	console.assert(!isNaN(getValue(3, 3)), "exemple4_4", getValue(3, 3));
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	tests();
+	newGame();
+});
+
+document.body.addEventListener('keydown', (ev) => {
+	if(ev.key == 'ArrowUp')
+		haut();
+	else if(ev.key == 'ArrowDown')
+		bas();
+	else if(ev.key == 'ArrowLeft')
+		gauche();
+	else if(ev.key == 'ArrowRight')
+		droite();
+});
+
