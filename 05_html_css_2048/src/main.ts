98037c030d1be988005329f06046cab11cc3f226
Roann Cantel <roann.cantel@telecom-paris.fr>
2024-03-07 10:19:52 +0100

    TP05: Défi 6

--- 05_html_css_2048/src/main.ts
+++ 05_html_css_2048/src/main.ts
@@ -1,3 +1,6 @@
+//filename: 
+
+/*
 testBonjour();
 
 function bonjour(): void {
@@ -7,3 +10,31 @@ function bonjour(): void {
 function testBonjour(): void {
 	bonjour();
 }
+*/
+
+function haut(): void {
+	console.log("haut");
+}
+
+function bas(): void {
+	console.log("bas");
+}
+
+function gauche(): void {
+	console.log("gauche");
+}
+
+function droite(): void {
+	console.log("droite");
+}
+
+document.body.addEventListener('keydown', (ev) => {
+	if(ev.key == 'ArrowUp')
+		haut();
+	else if(ev.key == 'ArrowDown')
+		bas();
+	else if(ev.key == 'ArrowLeft')
+		gauche();
+	else if(ev.key == 'ArrowRight')
+		droite();
+});
